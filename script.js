05bd1dd44219db0a35baf8fa7ca5cbf420edc5ce
jashvendra2001 <86592846+jashvendra2001@users.noreply.github.com>
2026-09-08 22:07:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function firstWord(s) {
   // your code here
+
+	console.log(s[0])
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
-alert(firstWord(s));
+alert(firstWord(s)
+
+
+				
