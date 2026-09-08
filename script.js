75a69c3c5b61e8274c83d2bbe01a42cc36a5f38a
jashvendra2001 <86592846+jashvendra2001@users.noreply.github.com>
2026-09-08 22:40:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 function firstWord(s) {
-  // your code here
-	
+    let str = s.trimStart();
+    let index = str.indexOf(" ");
 
-	let str=s.trimStart()
-	let index =str.indexOf(" ")
-	
-	if(index=== -1)
-	{
-		return str[0]
-	}
+    if (index === -1) {
+        return str;
+    }
 
-	return str.substring(0,index)
+    return str.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
-alert(firstWord(s))
+alert(firstWord(s));
 
 
 				
